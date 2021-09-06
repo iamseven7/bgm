0b6388862554d63eb6a7d16f0dc933a56a326fc6
iamseven7 <90218618+iamseven7@users.noreply.github.com>
2021-09-06 23:09:47 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+begin = new Date(2021, 9, 2)
+console.log(begin)
+today = new Date()
+console.log(today)
+diff = today - begin
+document.getElementById("h1").innerHTML = " " + String(parseInt(diff / (24 * 60 * 60 * 1000))) + " gün oldu"
